89e5b0107b80bbe12854bc4b1e908c5ed6240b66
Dana Howell <howelld115@gmail.com>
2023-10-28 18:52:48 -0600

    test: Add all tests for valid placement

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -31,4 +31,44 @@ RSpec.describe do
       expect(board.valid_coordinate?("A22")).to eq(false)
     end
   end
+
+  describe "#valid_placement?" do
+    it "can tell if the coordinate array is the same as the ship length" do
+      board = Board.new
+      cruiser = Ship.new("Cruiser", 3)
+      submarine = Ship.new("Submarine", 2)  
+
+      expect(board.valid_placement?(cruiser, ["A1", "A2"])).to eq(false)
+      expect(board.valid_placement?(submarine, ["A2", "A3", "A4"])).to eq(false)
+    end
+
+    it "will check to see that coordinates are consecutive" do
+      board = Board.new
+      cruiser = Ship.new("Cruiser", 3)
+      submarine = Ship.new("Submarine", 2)    
+
+      expect(board.valid_placement?(cruiser, ["A1", "A2", "A4"])).to eq(false)
+      expect(board.valid_placement?(submarine, ["A1", "C1"])).to eq(false)
+      expect(board.valid_placement?(cruiser, ["A3", "A2", "A1"])).to eq(false)
+      expect(board.valid_placement?(submarine, ["C1", "B1"])).to eq(false)
+    end
+
+    it "will assure that coordinates are not diagonal" do
+      board = Board.new
+      cruiser = Ship.new("Cruiser", 3)
+      submarine = Ship.new("Submarine", 2) 
+      
+      expect(board.valid_placement?(cruiser, ["A1", "B2", "C3"])).to eq(false)
+      expect(board.valid_placement?(submarine, ["C2", "D3"])).to eq(false)
+    end
+
+    it "assures placement is still valid after meeting all previous placement requirements" do
+      board = Board.new
+      cruiser = Ship.new("Cruiser", 3)
+      submarine = Ship.new("Submarine", 2)    
+
+      expect(board.valid_placement?(submarine, ["A1", "A2"])).to eq(true)
+      expect(board.valid_placement?(cruiser, ["B1", "C1", "D1"])).to eq(true)
+    end
+  end
 end
