4d0042ad92b4e25bc055719f59f95fd2429ad96b
Yain Porter <yainporter@gmail.com>
2023-10-29 16:01:58 -0700

    feat: Create helper methods for #valid_placement? and helper method #cell_array, and refactor @cells so that it uses #cell_array to populate, and write the test for #place_ship

--- lib/board.rb
+++ lib/board.rb
@@ -2,30 +2,14 @@ class Board
   attr_reader :cells
 
   def initialize
-    @cells = {
-      "A1": Cell.new("A1"),
-      "A2": Cell.new("A2"),
-      "A3": Cell.new("A3"),
-      "A4": Cell.new("A4"),
-      "B1": Cell.new("B1"),
-      "B2": Cell.new("B2"),
-      "B3": Cell.new("B3"),
-      "B4": Cell.new("B4"),
-      "C1": Cell.new("C1"),
-      "C2": Cell.new("C2"),
-      "C3": Cell.new("C3"),
-      "C4": Cell.new("C4"),
-      "D1": Cell.new("D1"),
-      "D2": Cell.new("D2"),
-      "D3": Cell.new("D3"),
-      "D4": Cell.new("D4"),
-    }
+    @cells = {}
+    cell_array.each {|cell| @cells.store(cell, Cell.new(cell))}
   end
 
   def valid_coordinate?(coordinate)
     true_or_false = false
     @cells.each do |key,value|
-      if key.to_s == coordinate 
+      if key.to_s == coordinate && value.empty?
         true_or_false = true
       end
     end
@@ -33,13 +17,86 @@ class Board
   end
 
   def valid_placement?(ship, coordinates)
-    require 'pry' ; binding.pry
-    if coordinates.count == ship.length
-
+    # if coordinates == ["A1", "A2"] then require 'pry'; binding.pry end
+    if coordinates.count == ship.length && down?(coordinates)
+      true
+    elsif coordinates.count == ship.length && across?(coordinates)
+      true
     else
-      false 
+      false
     end
   end
+
+
+    # def valid_placement?(ship, coordinates)
+  #   true_or_false = nil
+  #   if coordinates.count == ship.length
+  #     across = check_index(find_index_array_across(coordinates))
+  #     down = check_index(find_index_array_down(coordinates))
+  #     if across.include?(false) && down.include?(false)
+  #       true_or_false = false
+  #     elsif across.include?(false) && check_down(coordinates) == false
+  #       true_or_false = false
+  #     elsif down.include?(false) && across.include?(false) == false
+  #       true_or_false = true
+  #     elsif across.include?(false) && down.include?(false) == false
+  #       true_or_false = true
+  #     end
+  #   else
+  #     true_or_false = false
+  #   end
+  #   true_or_false
+  # end
+
+  # def find_index_array_across(coordinates)
+  #   index_array = []
+  #   coordinates.each do |coordinate|
+  #     index_array << cell_array.find_index(coordinate)
+  #   end
+  #   index_array
+  # end
+
+  # def check_index(array)
+  #   next_index = nil
+  #   array.map do |index|
+  #     if next_index == nil
+  #       next_index = index.next
+  #     else
+  #       if index == next_index
+  #         next_index = index.next
+  #         true
+  #       else
+  #         next_index = index.next
+  #         false
+  #       end
+  #     end
+  #   end
+  # end
+
+  # def find_index_array_down(coordinates)
+  #   letter_array = []
+  #   coordinates.each do |coordinate|
+  #     letter_array << coordinate.chop
+  #   end
+  #   letter_array
+  # end
+
+  # def check_down(coordinates)
+  #   first_chr = []
+  #   last_chr = []
+  #   coordinates.each do |coordinate|
+  #     first_chr << coordinate.chr
+  #     last_chr << coordinate.slice(-1)
+  #   end
+  #   if last_chr.uniq.count == 1 && check_index(first_chr).include?(false) == false
+  #     true
+  #   else 
+  #     false
+  #   end
+  # end
+
+####################### PRIVATE METHODS START ###########################
+  private
 
   def cell_array
     cell_array = []
@@ -50,4 +107,74 @@ class Board
     end
     cell_array.sort
   end
+
+  def down?(coordinates)
+    if same_second_char?(coordinates) && consecutive_first_char?(coordinates)
+      true
+    end
+  end
+
+  def across?(coordinates)
+    if consecutive_second_char?(coordinates) && same_first_char?(coordinates)
+      true
+    end
+  end
+
+  def consecutive_first_char?(coordinates)
+    first_char = first_char(coordinates)
+    first_char == (first_char[0]..first_char[-1]).to_a
+  end
+
+  def consecutive_second_char?(coordinates)
+    second_char = second_char(coordinates)
+    second_char.map {|char| char.to_i} == (second_char[0].to_i..second_char[-1].to_i).to_a
+  end
+
+  def same_first_char?(coordinates)
+    first_char = first_char(coordinates)
+    first_char.uniq.count == 1
+  end
+
+  def same_second_char?(coordinates)
+    second_char = second_char(coordinates)
+    second_char.uniq.count == 1
+  end
+
+  def first_char(coordinates)
+    first_char = []
+    coordinates.each do |coordinate|
+      first_char << coordinate[0]
+    end
+    first_char
+  end
+
+  def second_char(coordinates)
+    second_char = []
+    coordinates.each do |coordinate|
+      second_char << coordinate[1]
+    end
+    second_char
+  end
+#   sub =  ["A1", "A2"]
+#   submarine =  ["A1", "C1"]
+#   cruiser = ["A3", "A2", "A1"]
+#   index_array = []
+#   sub.each do |coordinate|
+#     index_array << cell_array.find_index(coordinate)
+#   end
+#  next_index = nil
+#   index_array.map do |index|
+#     if next_index == nil
+#       next_index = index += 1
+#     else
+#       if index == next_index
+#         next_index = index += 1
+#         true
+#       else
+#         next_index = index += 1
+#         false
+#       end
+#     end
+#   end
+#   require 'pry'; binding.pry
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,4 +1,6 @@
 require './lib/board'
+require './lib/cell'
+require './lib/ship'
 
 RSpec.describe do
   describe "#initialize" do 
@@ -32,7 +34,7 @@ RSpec.describe do
     end
   end
 
-  describe "#valid_placement?" do
+  describe "#valid_placement??" do
     it "can tell if the coordinate array is the same as the ship length" do
       board = Board.new
       cruiser = Ship.new("Cruiser", 3)
@@ -69,6 +71,22 @@ RSpec.describe do
 
       expect(board.valid_placement?(submarine, ["A1", "A2"])).to eq(true)
       expect(board.valid_placement?(cruiser, ["B1", "C1", "D1"])).to eq(true)
+    end
+  end
+
+  describe '#place_ship' do
+    it 'can place a ship within multiple cells' do
+      board = Board.new
+      cruiser = Ship.new("Cruiser", 3) 
+      cell_1 = board.cells["A1"] 
+      cell_2 = board.cells["A2"]
+      cell_3 = board.cells["A3"]
+      board.place(cruiser, ["A1", "A2", "A3"]) 
+
+      expect(cell_1.ship).to eq(cruiser)
+      expect(cell_2.ship).to eq(cruiser)
+      expect(cell_3.ship).to eq(cruiser)
+      expect(cell_3.ship == cell_2.ship).to eq(true)
     end
   end
 end
