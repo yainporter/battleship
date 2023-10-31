28a9b5eb7d36dd37d86972c69712955ebb6ea3db
Yain Porter <144638203+yainporter@users.noreply.github.com>
2023-10-30 22:05:39 -0700

    Add #board_text from Game to Message and change name to #board_header

--- lib/message.rb
+++ lib/message.rb
@@ -1,3 +1,13 @@
 class Message
+    
+  def board_header(player_or_computer)
+    if player_or_computer == "computer"
+      "=============COMPUTER BOARD============="
+    elsif player_or_computer == "player"
+      "==============PLAYER BOARD=============="
+    else
+      "Try again, please."
+    end
+  end
 
 end
