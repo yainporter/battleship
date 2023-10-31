2523614c344c23c1fa9447a3575b516112dc1c2e
Yain Porter <yainporter@gmail.com>
2023-10-31 08:24:56 -0700

    test: Reorganize and edit code for Message to work with the spec

--- lib/message.rb
+++ lib/message.rb
@@ -1,38 +1,17 @@
 class Message
 
-  
+  def initialize
+  end
 
-  def main_menu
+  def main_menu_msg
     "Welcome to BATTLESHIP\nEnter p to play. Enter q to quit."
-  end
-    
-  def board_header(player_or_computer)
-    if player_or_computer == "computer"
-      "=============COMPUTER BOARD============="
-    elsif player_or_computer == "player"
-      "==============PLAYER BOARD=============="
-    else
-      "Try again, please."
-    end
-  end
-  
-    def player_shot_prompt
-    "Enter the coordinate for your shot:"
-  end
-
-  def invalid_coordinates_msg
-    "Those are invalid coordinates. Please try again:\n >"
-  end
-
-  def exit
-    "Thanks for playing, see you soon."
   end
 
   def setup_msg
     "I have laid out my ships on the grid.\n You now need to lay out your two ships.\n The Cruiser is three units long and the Submarine is two units long.\n"
   end
-  
-  def ship_message(ship)
+
+  def ship_msg(ship)
     if ship == "cruiser"
       "Enter the squares for the Cruiser (3 spaces):\n >" 
     elsif ship == "submarine"    
@@ -42,4 +21,25 @@ class Message
     end
   end
 
+  def invalid_coordinates_msg
+    "Those are invalid coordinates. Please try again:\n >"
+  end
+
+  def board_header_msg(player_or_computer)
+    if player_or_computer == "computer"
+      "=============COMPUTER BOARD============="
+    elsif player_or_computer == "player"
+      "==============PLAYER BOARD=============="
+    else
+      "Try again, please."
+    end
+  end
+  
+    def player_shot_prompt_msg
+    "Enter the coordinate for your shot:"
+  end
+
+  def exit_msg
+    "Thanks for playing, see you soon."
+  end
 end
--- spec/message_spec.rb
+++ spec/message_spec.rb
@@ -1,4 +1,4 @@
-require './spec/spec_help'
+require './spec/spec_helper'
 
 RSpec.describe Message do
   describe 'initialize' do
@@ -11,47 +11,43 @@ RSpec.describe Message do
 
   describe '#main_menu' do
     it 'can print the #main_menu welcome message' do
-    game_msg = Message.new
+      msg = Message.new
 
-      (game_msg.main_menu).to eq("Welcome to BATTLESHIP\nEnter p to play. Enter q to quit.")
+      expect(msg.main_menu_msg).to eq("Welcome to BATTLESHIP\nEnter p to play. Enter q to quit.")
     end
   end
 
-#   describe 'setup_msg' do
-#     it 'directs the player what to do after setting the computers board' do
-#     player_turn = Turn.new("Player")
+  describe 'setup_msg' do
+    it 'directs the player what to do after setting the computers board' do
+    msg = Message.new
 
-#     expect(player_turn_game.setup_msg).to eq("I have laid out my ships on the grid.\n You now need to lay out your two ships.\n The Cruiser is three units long and the Submarine is two units long.\n")
-#     #   1 2 3 4\n
-#     # A . . . .\n
-#     # B . . . .\n
-#     # C . . . .\n
-#     # D . . . .\n")
-#     end
-#   end
+    expect(msg.setup_msg).to eq("I have laid out my ships on the grid.\n You now need to lay out your two ships.\n The Cruiser is three units long and the Submarine is two units long.\n")
+    end
+  end
 
-#   describe '#ship_message' do
-#     it 'generates a prompt for the user to place their cruiser or submarine' do
-#       player_turn = Turn.new("Player")
+  describe '#ship_message' do
+    it 'generates a prompt for the user to place their cruiser or submarine' do
+      msg = Message.new
 
-#       expect(player_turn_game.ship_message("cruiser")).to eq("Enter the squares for the Cruiser (3 spaces):\n >")
-#       expect(player_turn_game.ship_message("submarine")).to eq("Enter the squares for the Submarine(2 spaces):\n > ")
-#       expect(player_turn_game.ship_message("ship")).to eq("Error, try again")
-#     end
-#   end
+      expect(msg.ship_msg("cruiser")).to eq("Enter the squares for the Cruiser (3 spaces):\n >")
+      expect(msg.ship_msg("submarine")).to eq("Enter the squares for the Submarine(2 spaces):\n > ")
+      expect(msg.ship_msg("ship")).to eq("Error, try again")
+    end
+  end
 
-#   describe '#invalid_coordinates' do
-#     it 'can tell the user that their coordinates are invalid, and prompt them to try again' do
-#       player_turn = Turn.new("Player", "A1, B2, C3") 
+  describe '#invalid_coordinates' do
+    it 'can tell the user that their coordinates are invalid, and prompt them to try again' do
+      msg = Message.new
 
-#       expect(player_turn_game.invalid_coordinates_msg).to eq("Those are invalid coordinates. Please try again:\n >")
-#     end
-#   end
+      expect(msg.invalid_coordinates_msg).to eq("Those are invalid coordinates. Please try again:\n >")
+    end
+  end
 
-#   describe 'exit' do
-#     it 'can end the turn game' do
-#       player_turn = Turn.new("Player")
-#       expect(player_turn_game.exit).to eq("Thanks for playing, see you soon.")
-#     end
-#   end
+  describe 'exit' do
+    it 'can end the turn game' do
+      msg = Message.new
+
+      expect(msg.exit_msg).to eq("Thanks for playing, see you soon.")
+    end
+  end
 end
