0ad3c6a586d7ee0809bd22673e08d81cb4a1c318
Yain Porter <144638203+yainporter@users.noreply.github.com>
2023-10-30 22:09:29 -0700

    Add default messages from Turn

--- lib/message.rb
+++ lib/message.rb
@@ -9,5 +9,35 @@ class Message
       "Try again, please."
     end
   end
+  
+    def player_shot_prompt
+    "Enter the coordinate for your shot:"
+  end
+
+  def invalid_coordinates_msg
+    "Those are invalid coordinates. Please try again:\n >"
+  end
+
+  def exit
+    "Thanks for playing, see you soon."
+  end
+
+  def setup_msg
+    "I have laid out my ships on the grid.\n You now need to lay out your two ships.\n The Cruiser is three units long and the Submarine is two units long.\n"
+  end
+  
+  def ship_message(ship)
+    if ship == "cruiser"
+      "Enter the squares for the Cruiser (3 spaces):\n >" 
+    elsif ship == "submarine"    
+      "Enter the squares for the Submarine(2 spaces):\n > "
+    else 
+      "Error, try again"
+    end
+  end
+  
+  def main_menu
+    "Welcome to BATTLESHIP\nEnter p to play. Enter q to quit."
+  end
 
 end
